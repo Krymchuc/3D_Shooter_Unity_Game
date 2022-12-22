22ee04f2b60b654ad14268ccb05a676a6c5c5cdb
Krymchuc <khrystyna.krymchuk@oa.edu.ua>
2022-12-22 12:47:53 +0200

    Bullets script and audio

---
+++ Simple-FPS-Game-master/Assets/Scripts/MagazineDestroy.cs
@@ -0,0 +1,48 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class MagazineDestroy : MonoBehaviour
+{
+    AudioSource audioSource;
+    public AudioClip afterClip;
+    public AudioClip afterClip2;
+    MeshRenderer mesh;
+    private void Start()
+    {
+        audioSource = GetComponent<AudioSource>();
+        mesh = GetComponent<MeshRenderer>();
+    }
+    private void OnTriggerEnter(Collider other)
+    {
+        if(other.name == "Player")
+        {
+            //взяти патрони, щоб скинути магазин до 30
+            StopAllCoroutines();
+            StartCoroutine(wait());
+            PlayerFire.magazine = 30;
+            Text magazineText = GameObject.Find("Magazine").GetComponent<Text>();
+            magazineText.text = "Remaining bullet: " + PlayerFire.magazine;
+        }
+    }
+
+    IEnumerator wait()
+    {
+        mesh.enabled = false;
+        audioSource.Play();
+        print("1 execution");
+        yield return new WaitForSeconds(0.4f);
+        audioSource.Stop();
+        audioSource.clip = afterClip;
+        audioSource.Play();
+        print("2 execution");
+        yield return new WaitForSeconds(1.2f);
+        audioSource.Stop();
+        audioSource.clip = afterClip2;
+        audioSource.Play();
+        print("3 execution");
+        yield return new WaitForSeconds(0.6f);
+        Destroy(gameObject);
+    }
+}
---
+++ Simple-FPS-Game-master/Assets/Scripts/MagazineRotate.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MagazineRotate : MonoBehaviour
+{
+    Transform transform;
+    // Start is called before the first frame update
+    void Start()
+    {
+        transform = GetComponent<Transform>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        transform.rotation *= Quaternion.Euler(transform.rotation.x, 360 * Time.deltaTime, transform.rotation.z);
+    }
+}
