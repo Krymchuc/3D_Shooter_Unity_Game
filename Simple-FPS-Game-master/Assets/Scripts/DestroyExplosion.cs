31f520936f4f445c6aaf6a558ff8df0f35de6d7b
Krymchuc <khrystyna.krymchuk@oa.edu.ua>
2022-12-22 13:59:58 +0200

    Animation of enemy and player

---
+++ Simple-FPS-Game-master/Assets/Scripts/DestroyExplosion.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DestroyExplosion : MonoBehaviour
+{
+    public float destroyTime = 1.5f;
+    float currentTime = 0;
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (currentTime > destroyTime)
+        {
+            Destroy(gameObject);
+        }
+        currentTime += Time.deltaTime;
+    }
+}
---
+++ Simple-FPS-Game-master/Assets/Scripts/EndingScene.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EndingScene : MonoBehaviour
+{
+    public void MoveStart()
+    {
+        Application.LoadLevel("Start");
+    }
+    public void GameQuit()
+    {
+        Application.Quit();
+    }
+}
